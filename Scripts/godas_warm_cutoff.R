84a341d8a50f7eff8eaa629b31586bca8c1108ca
Breckie McCollum <bmccollum@berklee.edu>
2025-10-22 22:05:49 -0400

    new figure

--- Scripts/godas_warm_cutoff.R
+++ Scripts/godas_warm_cutoff.R
@@ -1,6 +1,7 @@
 library(readr)
 library(dplyr)
 library(tidyr)
+library(ggplot2)
 
 godas <- read_csv("Outputs/godas_allyears_5_15_25m.csv") |> 
   filter(MM %in% 7:9,
@@ -12,3 +13,24 @@ quantile(godas$godas_25, probs = 0.95, na.rm = TRUE) #17.6
 
 hist(godas$godas_15)
 hist(godas$godas_25)
+
+godas_monthly_long <- godas |>
+  pivot_longer(cols = -c(Date, YY, MM), names_to = "Depth", values_to = "Temperature")
+
+View(godas_monthly_long)
+
+ggplot(godas_monthly_long, aes(x = Date, y = Temperature, color = Depth)) +
+  geom_line(show.legend = FALSE)+
+  facet_wrap(~Depth)
+
+cbbPalette <- c("#E69F00", "#009E73", "#56B4E9")
+
+ggplot(transform(godas_monthly_long,
+                 Depth=factor(Depth,levels=c("godas_5","godas_15","godas_25"))),
+       aes(x = Date, y = Temperature, color = Depth)) +
+  scale_colour_manual(values=cbbPalette)+
+  theme_bw(base_size = 20)+
+  geom_line(show.legend = FALSE)+
+  labs(y = expression(paste("Temperature", "\u00b0C")))+
+  facet_wrap(~Depth)
+ggsave("Figures/godas_all_years_summer_plot.jpg")  
