2ae5d39b6e5fdf313af9d9ed6c04bb694460c1ad
Breckie McCollum <bmccollum@berklee.edu>
2025-10-28 12:51:07 -0400

    modelling temp changes at depth

--- Scripts/godas_warm_cutoff.R
+++ Scripts/godas_warm_cutoff.R
@@ -1,12 +1,25 @@
+#Make Mean summer temp plots at all godas depths
+
 library(readr)
 library(dplyr)
 library(tidyr)
 library(ggplot2)
 
+library(performance)
+library(car)
+library(broom)
+library(modelr)
+library(emmeans)
+
+library(GGally)
+library(visreg)
+library(piecewiseSEM)
 #filter data to just show summer months
 godas <- read_csv("Outputs/godas_allyears_5_15_25m.csv") |> 
   filter(MM %in% 7:9,
          YY %in% 1989:2024)
+
+View(godas)
 
 #determine 95% quantile temp thresholds
 quantile(godas$godas_15, probs = 0.95, na.rm = TRUE) #18.6
@@ -34,6 +47,11 @@ godas_monthly_long <- godas |>
 
 View(godas_monthly_long)
 
+godas_means <- godas_monthly_long |>
+  group_by(YY, Depth) |>
+  summarise_at(vars(Temperature), list(mean = mean))
+
+View(godas_means)
 #plot
 ggplot(godas_monthly_long, aes(x = Date, y = Temperature, color = Depth)) +
   geom_line(show.legend = FALSE)+
@@ -51,3 +69,33 @@ ggplot(transform(godas_monthly_long,
   labs(y = expression(paste("Temperature", "\u00b0C")))+
   facet_wrap(~Depth)
 ggsave("Figures/godas_all_years_summer_plot.jpg")  
+
+
+#plot means by depth
+ggplot(transform(godas_means,
+                 Depth=factor(Depth,levels=c("godas_5","godas_15","godas_25"))),
+       aes(x = YY, y = mean, color = Depth)) +
+  scale_colour_manual(values=cbbPalette)+
+  theme_bw(base_size = 20)+
+  geom_line(show.legend = FALSE)+
+  labs(y = expression(paste("Temperature", "\u00b0C")), x = expression(paste("Year")))+
+  facet_wrap(~Depth)
+ggsave("Figures/godas_summer_means_plot.jpg")  
+
+
+Temp_by_depth_and_yearlm <- lm(mean ~ Depth*YY,
+                               data = godas_means)
+
+check_model(Temp_by_depth_and_yearlm)
+
+
+check_model(Temp_by_depth_and_yearlm, check = "linearity") |> plot()
+
+tidy(Temp_by_depth_and_yearlm)
+temp_means_em <- emmeans(Temp_by_depth_and_yearlm,  ~Depth)
+
+contrast(temp_means_em, method = "pairwise") |>
+  confint()
+
+car::Anova(Temp_by_depth_and_yearlm) 
+emtrends(Temp_by_depth_and_yearlm, ~Depth, "YY")
